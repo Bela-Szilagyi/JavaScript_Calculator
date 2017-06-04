9cc4a84fd51ad0e2d398ed0ef542cca85a7ebb73
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-04 20:17:55 +0200

    clickable buttons

---
+++ JSCalculator.js
@@ -0,0 +1,65 @@
+var result = 0;
+var resultString
+var resultList
+document.getElementById('output').innerHTML = result;
+function main() {
+    $('#0').on('click', function() {
+        // alert('click');
+        result = 0
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#1').on('click', function() {
+        // alert('click');
+        result = 1
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#2').on('click', function() {
+        // alert('click');
+        result = 2
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#3').on('click', function() {
+        // alert('click');
+        result = 3
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#4').on('click', function() {
+        // alert('click');
+        result = 4
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#5').on('click', function() {
+        // alert('click');
+        result = 5
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#6').on('click', function() {
+        // alert('click');
+        result = 6
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#7').on('click', function() {
+        // alert('click');
+        result = 7
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#8').on('click', function() {
+        // alert('click');
+        result = 8
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#9').on('click', function() {
+        // alert('click');
+        result = 9
+        document.getElementById('output').innerHTML = result;
+    });
+    $('#C').on('click', function() {
+        // alert('click');
+        result = 0
+        document.getElementById('output').innerHTML = result;
+    });
+
+
+}
+
+$(document).ready(main);
